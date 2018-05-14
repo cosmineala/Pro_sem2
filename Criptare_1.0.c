d3deb3a015befe9850d99b7529e95b856162d1f5
cosmin_louis <32954953+cosmineala@users.noreply.github.com>
2018-05-14 21:53:04 +0300

    1.1

--- Criptare_1.0.c
+++ Criptare_1.0.c
@@ -2,9 +2,7 @@
 #include <string.h>
 
 
-int trans_asci_num [26] = { 10 , 23 , 21 , 12 , 2 , 13 , 14 , 15 , 7 , 16 , 17 , 18 , 25
-    , 24 , 8 ,  9  , 0 ,  3  , 11 , 4 ,  6 ,  22 , 1 ,  20 , 5  , 19 }; //65-90   97--122     uper-32
-
+int trans_asci_num [26] = { 10 , 23 , 21 , 12 , 2 , 13 , 14 , 15 , 7 , 16 , 17 , 18 , 25, 24 , 8 ,  9  , 0 ,  3  , 11 , 4 ,  6 ,  22 , 1 ,  20 , 5  , 19 }; //65-90   97--122
 int trans_mum_asci [26] = { 16 , 22 , 4 , 17 , 19 , 24 , 20 , 8 , 14 , 15 , 0 , 18 , 3 , 5 , 6 , 7 , 9 , 10 , 11 , 25 , 23 , 2 , 21 , 1 , 13 , 12 };//+97
 
 char st_citire[2000];
@@ -17,10 +15,10 @@ int main()
     int i = 1;
     
     scanf("%[^\n]s", st_citire);//citire sir
-
-  
+    
+    
     if ( st_citire[0] >= 'a' && st_citire[0] <= 'z') {
-         a =trans_asci_num [ ( int ) st_citire[0] - 97] ;
+        a =trans_asci_num [ ( int ) st_citire[0] - 97] ;
         if ( a < 10 || a > 19) {
             printf("Mesaj incorect , incercati din nou");
             return 0;
@@ -39,26 +37,21 @@ int main()
         
     }
     
-    /*int a = trans_asci_num [ ( int ) st_citire[0] - 97] ;
-    if ( a < 10 || a > 19) {
-        printf("Mesaj incorec , incercati dinou");
-        return 0;
-    }*/
-    
     
     a-=10;
     st_afisare[0]=st_citire[0];
+
     
-    for ( i = 1; i < strlen(st_citire); i++) {                         // printf(" >pas>%d<pas<  \n",i);
+    for ( i = 1; i < strlen(st_citire); i++) {
         
         if ( st_citire[i] >= 'a' && st_citire[i] <= 'z' ) {//-------------litere mici
             
             p = trans_asci_num [ (( int ) st_citire[i]) - 97];
-                                                                      //  printf(" >poz>%d<poz< \n",p);
-          if ( p >= 0 && p <= 9 ) {/*--------rand 1*/              //  printf(">rand1>%d<rand1<\n",p);
-                if ( p + a > 9 ) {                                 //    printf(">r1sub>%d<r1sub<\n",p);
+            //  printf(" >poz>%d<poz< \n",p);
+            if ( p >= 0 && p <= 9 ) {/*--------rand 1*/
+                if ( p + a > 9 ) {
                     p = p + a - 10;
-                    st_afisare[i] = (char)(trans_mum_asci[p] + 97);      //    printf(">>p=%d<<\n",p);//ver
+                    st_afisare[i] = (char)(trans_mum_asci[p] + 97);
                 }
                 else{
                     p = p + a;
@@ -91,37 +84,27 @@ int main()
             }
             
             
-        }else{                      // litere mari - end
-        
-        if ( st_citire[i] >= 'A' && st_citire[i] <= 'Z' ) {//-------------litere mari
+        }
+        else{                      // litere mari - end
             
-            p = trans_asci_num [ (( int ) st_citire[i]) - 65];
-            //  printf(" >poz>%d<poz< \n",p);
-            if ( p >= 0 && p <= 9 ) {/*--------rand 1*/              //  printf(">rand1>%d<rand1<\n",p);
-                if ( p + a > 9 ) {                                 //    printf(">r1sub>%d<r1sub<\n",p);
-                    p = p + a - 10;
-                    st_afisare[i] = (char)(trans_mum_asci[p] + 65);      //    printf(">>p=%d<<\n",p);//ver
-                }
-                else{
-                    p = p + a;
-                    st_afisare[i] = (char)(trans_mum_asci[p] + 65);}
+            if ( st_citire[i] >= 'A' && st_citire[i] <= 'Z' ) {//-------------litere mari
                 
-            }
-            else{
-                if ( p >= 10 && p <= 18 ) {//------rand 2
-                    if ( p + a > 18) {
-                        p = p + a - 9;
+                p = trans_asci_num [ (( int ) st_citire[i]) - 65];
+                //  printf(" >poz>%d<poz< \n",p);
+                if ( p >= 0 && p <= 9 ) {/*--------rand 1*/
+                    if ( p + a > 9 ) {
+                        p = p + a - 10;
                         st_afisare[i] = (char)(trans_mum_asci[p] + 65);
                     }
                     else{
                         p = p + a;
-                        st_afisare[i] = (char)(trans_mum_asci[p] + 65);
-                    }
+                        st_afisare[i] = (char)(trans_mum_asci[p] + 65);}
+                    
                 }
                 else{
-                    if ( p >= 19 && p <= 25 ) {//------rand 3
-                        if ( p + a > 25) {
-                            p = p + a - 7;
+                    if ( p >= 10 && p <= 18 ) {//------rand 2
+                        if ( p + a > 18) {
+                            p = p + a - 9;
                             st_afisare[i] = (char)(trans_mum_asci[p] + 65);
                         }
                         else{
@@ -129,29 +112,34 @@ int main()
                             st_afisare[i] = (char)(trans_mum_asci[p] + 65);
                         }
                     }
+                    else{
+                        if ( p >= 19 && p <= 25 ) {//------rand 3
+                            if ( p + a > 25) {
+                                p = p + a - 7;
+                                st_afisare[i] = (char)(trans_mum_asci[p] + 65);
+                            }
+                            else{
+                                p = p + a;
+                                st_afisare[i] = (char)(trans_mum_asci[p] + 65);
+                            }
+                        }
+                    }
                 }
+                
+                
+            }/*--------------------------------end-literemari*/
+            else{
+                
+                
+                st_afisare[i] = st_citire[i];
             }
-            
-            
-        }else{
-        
-       
-            st_afisare[i] = st_citire[i];
-          }
         }
         
-    
-        
-        
-        }                     // for-end
+    }                     // for-end
     
     printf(" \n%s\n",st_afisare);
-   // printf(" >citire>%s<sitre< /n",st_citire);
     
-  //  printf("  >a=>%d<a=< /ng",a);
+    return 0;
     
-    
-    
-    
-    return 0;}
+}
 
